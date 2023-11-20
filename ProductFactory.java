f9830a8e9e1698ca5c0eb141f956e7dddd3222e7
DHuber10 <47402664+DHuber10@users.noreply.github.com>
2023-11-20 16:45:11 -0500

    Update ProductFactory.java

--- ProductFactory.java
+++ ProductFactory.java
@@ -8,6 +8,8 @@ public class ProductFactory {
                 return new Clothing(id, name, price, "clothing specific attributes");
             case "electronics":
                 return new Electronics(id, name, price, "electronic specific attributes");
+            case "gaming":
+                return new Gaming(id, name, price, "gaming specific attributes"); 
             default:
                 throw new IllegalArgumentException("Unknown product type");
         }
